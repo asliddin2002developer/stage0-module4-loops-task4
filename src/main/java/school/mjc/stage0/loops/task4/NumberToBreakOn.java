dd83460b18fe6b7a3c86597623d81c86d0916dd1
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-11-24 14:06:33 +0500

    added comment

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        //loops
         for (int i = 1; i<=numberToGoUntil; i++){
             if (i > toBreakWith){
                 break;
